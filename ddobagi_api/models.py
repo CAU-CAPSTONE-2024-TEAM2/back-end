091c8398f7cc88ed84792ac1c668188c6418564d
coffeesigma <ljb22060@gmail.com>
2024-06-04 19:41:15 +0900

    question model: add grammar class, put data in sqlite

--- ddobagi_api/models.py
+++ ddobagi_api/models.py
@@ -18,8 +18,9 @@ class Question(models.Model):
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
     question_number = models.IntegerField()
     word = models.CharField(max_length=255)
-    incorrect_pronounciation = models.CharField(max_length=255)
     correct_pronounciation = models.CharField(max_length=255)
+    incorrect_pronounciation = models.CharField(max_length=255)
+    grammar_class = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.level.name} - {self.question_number}: {self.word}"
